875ff817fccffb9fb263fc5a89e2d79068629eb2
Ogurche <337398@edu.itmo.ru>
2024-10-25 17:59:20 +0000

    log

--- Scripts/copy_init.py
+++ Scripts/copy_init.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-level = "3" #Какую таблицу тестируем, маленькую, среднюю или большую
+level = "2" #Какую таблицу тестируем, маленькую, среднюю или большую
 your_bucket_name = "result" #Имя вашего бакета
 your_access_key = os.getenv('ACCESS') #Ключ от вашего бакета
 your_secret_key = os.getenv('SECRET') #Ключ от вашего бакета
